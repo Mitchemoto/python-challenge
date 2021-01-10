2abe64b14ec83ec1730fc4c420b820993193194b
Mitchell <erich.mitchell87@gmail.com>
2021-01-09 23:25:56 -0700

    --test

--- PyPoll/mainPoll.py
+++ PyPoll/mainPoll.py
@@ -18,10 +18,13 @@ csv_header=next(csvreader)
 # Will need to make variable lists for each item to return from the db. 
 #voter_id = []
 #county_list=[]
-candidate=[]
+all_candidates=[]
 #dup_cand=[]
 votes_cast=0
 total_votes=[]
+vote_percent=[]
+winner_index=0
+percentage_index=[]
 
 with open(file_path,'r'):
     csvreader=csv.reader(csvfile,delimiter=",")
@@ -29,11 +32,11 @@ with open(file_path,'r'):
     
 #For loop using an if statement to total votes for a candidate
 #use append to remove duplicates
-    for row in csvreader:
+    for row in csv_header:
         votes_cast=votes_cast+1
         candidate=csvreader[2]
         if candidate in all_candidates:
-            cand_index=candidates.index(candidate)
+            cand_index=all_candidates.index(candidate)
             total_votes[cand_index]=total_votes[cand_index]+1
         else:
             cand_index.append(candidate)
@@ -45,14 +48,21 @@ with open(file_path,'r'):
 
 # A complete list of candidates who received votes
 # remove duplicate values from the candidate list
-# start a for loop for csvreader to remove the duplicate candidate values
-# use set to remove dulicates
-#dup_cand=[]
-    for row in csvreader:
-        
+# start a for loop for csvreader with an if/else staement to get the percentage of vote
+# as well as the winner
+most_votes=total_votes[0]
+for votes in range(len(all_candidates)):
+    vote_percent=total_votes[votes]/votes_cast*100
+    percentage_index.append(vote_percent)
+    if total_votes[votes] > most_votes:
+        most_votes=total_votes[votes]
+        print(most_votes)
+        winner_index=votes
+winner=winner_index[most_votes]
 
+        
 #create a dict to store the candidates
-  #  PyPoll={}
+#  PyPoll={}
 
 # The total number of votes each candidate won
 # Start a for loop through the total_candidates in dup_cand, then get the percentage using round and an equation 
@@ -64,8 +74,7 @@ with open(file_path,'r'):
         # The percentage of votes each candidate won
 #        vote_percent=float(PyPoll(total_candidates[y]) / votes_cast * 100)
         
-
- #   print (candidate + ": " + str(vote_percent) + "% (" + str(sum_candidates) + ")")
+#   print (candidate + ": " + str(vote_percent) + "% (" + str(sum_candidates) + ")")
 # The winner of the election based on popular vote.
 
 
@@ -85,5 +94,8 @@ print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {votes_cast}")
 print("-------------------------")
-for votes in range(len(total_candidates)):
-    print (f"{candidate} : {vote_percent}% :({total_candidates})")
+for votes in range(len(all_candidates)):
+    print (f"{candidate[votes]} : {vote_percent[votes]}% :({all_candidates[votes]})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
